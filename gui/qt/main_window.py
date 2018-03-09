52c5a0f4731b5dcd6ae5cb8f3af9660e400cb56b
Sirak Ghazaryan <sirak@crown.tech>
2018-03-10 00:52:21 +0400

    Fix uri for donation server

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -542,7 +542,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         d = self.network.get_donation_address()
         if d:
             host = self.network.get_parameters()[0]
-            self.pay_to_URI('bitcoin:%s?message=donation for %s'%(d, host))
+            self.pay_to_URI('crown:%s?message=donation for %s'%(d, host))
         else:
             self.show_error(_('No donation address for this server'))
 
